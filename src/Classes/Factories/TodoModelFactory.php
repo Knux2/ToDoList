2b9c603472a7652fd6d048b145caca263e9e333a
Knux2 <ben_ashfield@hotmail.com>
2019-05-09 11:20:02 +0100

    Todo Model Factory created with invoke function

--- src/Classes/Factories/TodoModelFactory.php
+++ src/Classes/Factories/TodoModelFactory.php
@@ -1,3 +1,14 @@
 <?php
 
 namespace Portal\Factories;
+
+use Portal\Model\TodoModel;
+
+Class TodoModelFactory
+{
+    public function __invoke($container)
+    {
+        $db = $container['dbConnection'];
+        return new TodoModel($db);
+    }
+}
