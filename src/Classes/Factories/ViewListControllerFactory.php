447d3b90b15d972a737d477d6c488743ddd6d3a5
Knux2 <ben_ashfield@hotmail.com>
2019-05-09 11:28:33 +0100

    Added container interface to ViewListControllerFactory

--- src/Classes/Factories/ViewListControllerFactory.php
+++ src/Classes/Factories/ViewListControllerFactory.php
@@ -1,3 +1,16 @@
 <?php
 
 namespace Portal\Factories;
+
+use Psr\Container\ContainerInterface;
+
+Class ViewListControllerFactory
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        $renderer = $container->get('renderer');
+        $todoModel = $container->get('todoModel');
+        $dbConnection = $container->get('dbConnection');
+        return new \Portal\Controller\ViewListController($renderer, $todoModel, $dbConnection);
+    }
+}
