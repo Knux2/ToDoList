798b99be7c07dbd69b18dac19ddb962dc8645493
Knux2 <ben_ashfield@hotmail.com>
2019-05-09 15:21:00 +0100

    Removed renderer and dbConnecyion from AddTaskController Factory

--- src/Classes/Factories/AddTaskControllerFactory.php
+++ src/Classes/Factories/AddTaskControllerFactory.php
@@ -8,9 +8,7 @@ Class AddTaskControllerFactory
 {
     public function __invoke(ContainerInterface $container)
     {
-        $renderer = $container->get('renderer');
         $todoModel = $container->get('todoModel');
-        $dbConnection = $container->get('dbConnection');
-        return new \Portal\Controller\AddTaskController($renderer, $todoModel, $dbConnection);
+        return new \Portal\Controller\AddTaskController($todoModel);
     }
 }
