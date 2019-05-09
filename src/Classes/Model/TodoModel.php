8f49dfcfdf2f6b00bccf41d72df9ed9266401e11
Knux2 <ben_ashfield@hotmail.com>
2019-05-09 11:13:25 +0100

    Db Connection and SQL Query added to TodoModel, will retrieve list from db

--- src/Classes/Model/TodoModel.php
+++ src/Classes/Model/TodoModel.php
@@ -1,3 +1,20 @@
 <?php
 
 namespace Portal\Model;
+
+Class TodoModel
+{
+    private $dbConnection;
+
+    public function __construct($db)
+    {
+        $this->dbConnection = $db;
+    }
+
+    public function getList()
+    {
+        $query = $this->dbConnection->prepare("SELECT `id`, `task` FROM `list_table`");
+        $query->execute();
+        return $query->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
