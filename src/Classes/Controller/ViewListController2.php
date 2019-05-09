6b11fe39d8b54cbdf0c4c7ec8d5876c3cbd6ab2a
Knux2 <ben_ashfield@hotmail.com>
2019-05-09 11:42:51 +0100

    New Controller to View List created

---
+++ src/Classes/Controller/ViewListController2.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Portal\Controller;
+
+use Portal\Model\TodoModel;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Views\PhpRenderer;
+
+Class ViewListController
+{
+    private $renderer;
+    private $todoModel;
+
+    public function __construct(PhpRenderer $renderer, TodoModel $todoModel)
+    {
+        $this->renderer = $renderer;
+        $this->todoModel = $todoModel;
+    }
+
+    public function __invoke(Request $request, Response $response, $args)
+    {
+        $args['tasks'] = $this->todoModel->getList();
+        $this->renderer->render($response, 'todoList.phtml', $args);
+    }
+}
